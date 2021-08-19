8d0fc97c3fc0d62aa854d116394e53134213f7d4
jfcr-oficina <jfcrelectronico@gmail.com>
2021-08-19 13:00:10 -0500

    Added reto3

---
+++ src/reto3/Empleado.java
@@ -0,0 +1,45 @@
+//package reto2;
+
+public class Empleado {
+    protected int id;
+    protected String nombre;  
+    protected String tipoContrato;
+    protected int salario;
+    final protected double auxilio =106454;
+  
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }    
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+    public void setTipoContrato(String tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+    public int getSalario() {
+        return salario;
+    }
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+    public Empleado(String nombre,String tipoContrato,int salario)
+    {
+        setNombre(nombre);
+        setTipoContrato(tipoContrato);
+        setSalario(salario);
+    }
+    public double liquidarSalud(Empleado empleado)
+    {
+        return (empleado.salario*0.085+empleado.salario*0.12+empleado.salario*0.00522);
+    } 
+   
+}
